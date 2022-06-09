2aaa468e1776931d4080abef0e0d57ff593b0f8a
Dinko <dinko.dugec@gmail.com>
2022-06-10 01:15:45 +0200

    search function

--- app/data/mysqldataprovider.class.php
+++ app/data/mysqldataprovider.class.php
@@ -17,7 +17,7 @@ class MySqlDataProvider extends DataProvider {
 
         return $data;
     }
-    
+
     public function get_term($term) {
         $db = $this->connect();
 
@@ -44,8 +44,28 @@ class MySqlDataProvider extends DataProvider {
         return $data[0];
     }
     
-    public function search_terms($search) {
-            
+   public function search_terms($search) {
+        $db = $this->connect();
+
+        if ($db == null) {
+            return [];
+        }
+
+        $sql = 'SELECT * FROM terms WHERE term LIKE :search OR definition LIKE :search';
+        $smt = $db->prepare($sql);
+
+        // style%
+
+        $smt->execute([
+            ':search' => '%'.$search.'%',
+        ]);
+
+        $data = $smt->fetchAll(PDO::FETCH_CLASS, 'GlossaryTerm');
+
+        $smt = null;
+        $db = null;
+
+        return $data;
     }
     
     public function add_term($term, $definition) {
